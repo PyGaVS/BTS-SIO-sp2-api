ae8fdcfde71b3a884d9d3b3ad9f22409b95783ac
Alexandre BOURGUIGNON <bourgu20al@lycee-ndduroc.com>
2024-03-14 16:54:40 +0100

    Initial commit

---
+++ app/Http/Controllers/CerificationRequestController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCerificationRequestRequest;
+use App\Http\Requests\UpdateCerificationRequestRequest;
+use App\Models\CerificationRequest;
+
+class CerificationRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCerificationRequestRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CerificationRequest $cerificationRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CerificationRequest $cerificationRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCerificationRequestRequest $request, CerificationRequest $cerificationRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CerificationRequest $cerificationRequest)
+    {
+        //
+    }
+}
