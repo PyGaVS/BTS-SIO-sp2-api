cf737d0d27df8882b49f7f18449014ee8a301798
Lylian BALL <BALL27LY@lycee-ndduroc.com>
2024-03-21 17:06:03 +0100

    auth with username

--- app/Http/Controllers/v1/AuthController.php
+++ app/Http/Controllers/v1/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends BaseController
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required',
+            'username' => 'required',
             'email' => 'required|email',
             'password' => 'required',
         ]);
@@ -32,7 +32,7 @@ class AuthController extends BaseController
         Log::debug('inputs :', $input);
         $user = User::factory()->create($input);
         $success['token'] =  $user->createToken('MyApp')->plainTextToken;
-        $success['name'] =  $user->name;
+        $success['username'] =  $user->username;
 
         return $this->sendResponse($success, 'UserController register successfully.');
     }
@@ -40,7 +40,7 @@ class AuthController extends BaseController
 
     public function login(Request $request)
     {
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+        if(Auth::attempt(['username' => $request->username, 'password' => $request->password])){
             $user = Auth::user();
             $success['token'] =  $user->createToken('MyApp')->plainTextToken;
             return $this->sendResponse($success, 'UserController login successfully.');
